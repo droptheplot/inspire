a3b744311c3c92b2da6799f10608c6decbd212e3
droptheplot <novikov359@gmail.com>
2016-07-15 12:16:26 +0300

    Allow different models for ApplyRulesJob

--- app/jobs/apply_rules_job.rb
+++ app/jobs/apply_rules_job.rb
@@ -1,17 +1,20 @@
 class ApplyRulesJob < ApplicationJob
   queue_as :default
 
-  def perform(publication_id)
-    publication = Publication.find(publication_id)
+  # @param klass [ActiveRecord::Base]
+  # @param id [Integer]
+  def perform(klass, id)
+    obj = klass.find(id)
 
-    return unless publication
+    return unless obj
 
     rules = Rule.all
 
     rules.each do |rule|
-      return unless publication.content.include?(rule.keyword)
+      return unless obj.public_send(klass::APPLY_RULES_COLUMN_NAME)
+                       .include?(rule.keyword)
 
-      CreateTagService.new(publication, rule.tag.name).()
+      CreateTagService.new(obj, rule.tag.name).()
     end
   end
 end
