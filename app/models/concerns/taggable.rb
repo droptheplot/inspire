1cb22d5164531c639e983f713617b3bef19408dd
droptheplot <novikov359@gmail.com>
2016-07-15 12:46:58 +0300

    Fix Taggable concern for STI classes

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -5,9 +5,19 @@ module Taggable
     Tag.includes(:taggings)
        .where(
          taggings: {
-           taggable_type: self.class.to_s,
+           taggable_type: get_taggable_type,
            taggable_id: id
          }
        )
   end
+
+  private
+
+    def get_taggable_type
+      (namespaced? ? self.class.superclass : self.class).name
+    end
+
+    def namespaced?
+      self.class.name.include?('::')
+    end
 end
