1ab2b906c27abf5731d626dfb3c923e9bba3c57d
droptheplot <novikov359@gmail.com>
2016-07-15 16:15:19 +0300

    Fix polymorphic tags

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -2,7 +2,7 @@ module Taggable
   extend ActiveSupport::Concern
 
   included do
-    has_many :taggings, foreign_key: :taggable_id
+    has_many :taggings, as: :taggable
     has_many :tags, through: :taggings
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,9 @@
 class Tag < ApplicationRecord
   has_many :taggings
+  has_many :publications, through: :taggings, source: :taggable,
+           source_type: 'Publication'
+  has_many :authors, through: :taggings, source: :taggable,
+           source_type: 'Author'
 
   validates :name, presence: true, uniqueness: true
 end
