926645110e8e5bc06869e8fd98b0e2baf706ccdc
droptheplot <novikov359@gmail.com>
2016-07-15 08:28:56 +0300

    Add tags method to publications

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -2,4 +2,14 @@ class Publication < ApplicationRecord
   belongs_to :author
 
   validates :title, presence: true
+
+  def tags
+    Tag.includes(:taggings)
+       .where(
+         taggings: {
+           taggable_type: 'Publication',
+           taggable_id: id
+         }
+       )
+  end
 end
--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -1,4 +1,14 @@
-require 'rails_helper'
-
 describe Publication, type: :model do
+  let(:poem) { FactoryGirl.create(:publication_poem) }
+
+  describe '#tags' do
+    before do
+      CreateTagService.new(poem, 'first_tag').()
+      CreateTagService.new(poem, 'second_tag').()
+    end
+
+    it 'returns tags' do
+      expect(poem.tags.pluck(:name)).to eq(%w(first_tag second_tag))
+    end
+  end
 end
