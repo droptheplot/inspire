5ad56cbccea54d7e53494287824a83ebcc14ccd0
droptheplot <novikov359@gmail.com>
2016-07-15 12:13:40 +0300

    Fix spec for ApplyRulesJob

--- spec/jobs/apply_rules_job_spec.rb
+++ spec/jobs/apply_rules_job_spec.rb
@@ -1,31 +1,27 @@
 describe ApplyRulesJob, type: :job do
   describe '#perform' do
-    before do
+    before(:each) do
       tag = FactoryGirl.create(:tag, name: 'tag_for_keyword')
       FactoryGirl.create(:rule, tag: tag, keyword: 'keyword')
     end
 
     context 'with keyword' do
       let(:poem) do
-        FactoryGirl.create(:publication_poem, :with_keyword)
+        FactoryGirl.build(:publication_poem, :with_keyword)
       end
 
       it 'creates tag for publication' do
-        expect do
-          ApplyRulesJob.perform_now(poem.id)
-        end.to change { Tagging.count }.by(1)
+        expect { poem.save }.to change { Tagging.count }.by(1)
       end
     end
 
     context 'without keyword' do
       let(:poem) do
-        FactoryGirl.create(:publication_poem, :without_keyword)
+        FactoryGirl.build(:publication_poem, :without_keyword)
       end
 
-      it 'creates tag for publication' do
-        expect do
-          ApplyRulesJob.perform_now(poem.id)
-        end.not_to change { Tagging.count }
+      it 'do nothing' do
+        expect { poem.save }.not_to change { Tagging.count }
       end
     end
   end
